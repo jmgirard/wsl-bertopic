061e37cd76da399f486b5f82c79164cdc619a377
jmgirard <me@jmgirard.com>
2025-01-31 09:31:23 -0600

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM ghcr.io/astral-sh/uv:0.5.26-python3.9-bookworm
+
+ENV UV_PYTHON_VERSION=3.9
+
+RUN uv pip install bertopic --system
+
+COPY install_cudatoolkit.sh /setup_scripts/
+RUN chmod +x /setup_scripts/*.sh
+RUN /setup_scripts/install_cudatoolkit.sh
+
+CMD ["python3"]
---
+++ install_cudatoolkit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# install_cudatoolkit.sh
+
+set -e
+
+# A function to install apt packages only if they are not installed
+function apt_install() {
+    if ! dpkg -s "$@" >/dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            apt-get update
+        fi
+        apt-get install -y --no-install-recommends "$@"
+    fi
+}
+
+# Install apt dependencies
+apt_install \
+    build-essential \
+    wget
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-keyring_1.1-1_all.deb
+dpkg -i cuda-keyring_1.1-1_all.deb
+
+apt-get update
+apt_install cuda-toolkit-12-8
